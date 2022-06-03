85938fcffd229b2820a7a2541e83722e4290b7d5
Jean Carlos <jean-los@hotmail.com>
2022-06-02 23:33:20 -0300

    update

--- function.js
+++ function.js
@@ -1,1 +1,12 @@
+// Limpeza
 $(".barra-inicial").remove()
+$("#cabecalho > div.conteiner > div.row-fluid > div:nth-child(1) > div.superior.row-fluid").remove()
+$("#cabecalho > div.conteiner > div.row-fluid > div:nth-child(3) > div.superior.row-fluid > div > ul > li:nth-child(1)").remove()
+$("#cabecalho > div.conteiner > div.row-fluid > div:nth-child(3) > div.inferior.row-fluid > div > div > a > span:nth-child(3)").remove()
+
+if (modulo_tarja_texto) {
+    cab = $("#cabecalho")
+    tarja_text = []
+    tarja_text = modulo_tarja_texto.split(modulo_cupom)
+    cab.prepend('<div class="modulo_tarja">' + text[0] + '<span>' + modulo_cupom + '</span>' + text[1] + '</div>');
+}
