7292b9434007a0577c6f932d716665c25e22b29e
Jean Carlos <jean-los@hotmail.com>
2022-06-07 23:44:12 -0300

    up

--- function.js
+++ function.js
@@ -1,7 +1,7 @@
 // Limpeza
 $(".barra-inicial").remove()
 $("#cabecalho > div.conteiner > div.row-fluid > div:nth-child(1) > div.superior.row-fluid").remove()
-$("#cabecalho > div.conteiner > div.row-fluid > div:nth-child(3) > div.superior.row-fluid > div > ul > li:nth-child(1)").remove()
+$("#cabecalho > div.conteiner > div.row-fluid > div:nth-child(3) > div.superior.row-fluid").remove()
 $("#cabecalho > div.conteiner > div.row-fluid > div:nth-child(3) > div.inferior.row-fluid > div > div > a > span:nth-child(3)").remove()
 $("#cabecalho > div.conteiner > div.row-fluid > div:nth-child(3) > div.superior.row-fluid > div > ul > li > a").remove()
 
@@ -9,5 +9,8 @@ if (modulo_tarja_texto) {
     cab = $("#cabecalho")
     tarja_text = []
     tarja_text = modulo_tarja_texto.split(modulo_cupom)
-    cab.prepend('<div class="modulo_tarja"><i class="icon-lua"></i>' + tarja_text[0] + '<span> ' + modulo_cupom + ' </span>' + tarja_text[1] + '<i class="icon-pipa"></i></div>');
+    cab.prepend('<div class="modulo_tarja"><i class="top-icon1"></i>' + tarja_text[0] + ' <span> ' + modulo_cupom + ' </span> ' + tarja_text[1] + '<i class="top-icon2"></i></div>');
 }
+cart = $("#cabecalho > div.conteiner > div.row-fluid > div:nth-child(3) > div > div > div > a")
+cart.$(">i").removeClass().addClass("j-cart")
+cart($("").removeClass())
